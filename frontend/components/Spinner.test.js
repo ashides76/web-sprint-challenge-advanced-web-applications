576387a6eef2ba884aa8ce0ad6d67d0d4efb967d
Ashish Desai <desai.ashish76@gmail.com>
2024-04-29 11:41:39 -0400

    added spinner test cases

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,22 @@
 // Import the Spinner component into this file and test
+import React from "react";
+import Spinner from "./Spinner"
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import { render } from '@testing-library/react';
+import ArticleForm from './ArticleForm';
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
+
+test('renders with create mode heading', () => {
+  const { getByText } = render(<ArticleForm />);
+  const headingElement = getByText('Create Article');
+  expect(headingElement).toBeTruthy();
+});
+
+test('does not render spinner when "on" prop is false', () => {
+  const { container } = render(<Spinner on={false} />);
+  const spinnerElement = container.querySelector('.spinner');
+  expect(spinnerElement).toBeNull();
+});
+
